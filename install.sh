22ae52138fc797ff23613e711241325615338460
lizj <15014217605@163.com>
2022-07-31 00:38:56 +0800

    add sample

--- install.sh
+++ install.sh
@@ -1,1 +1,4 @@
 # How to write a dotfiles repository : ttps://dotfiles.github.io/tutorials/
+ln -sv "~/.dotfiles/runcom/.bash_profile" ~
+ln -sv "~/.dotfiles/runcom/.inputrc" ~
+ln -sv "~/.dotfiles/git/.gitconfig" ~
